5fe2b8fa5f18269771af13166ae6fc6b2f62ab87
Vadzim <vadzimk@hotmail.com>
2023-03-27 18:46:34 -0700

    add session authentication and login

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from account.forms import LoginForm
+from django.http import HttpResponse
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])  # checks credentials and returns user obj
+            if user is not None:
+                if user.is_active:
+                    login(request, user)  # sets the user in the current session
+                    return HttpResponse("Authenticated successfully")
+                else:
+                    return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
